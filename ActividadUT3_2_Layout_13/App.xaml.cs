4cf34bb5f0c3bf7fb669d25a030637d7aa00cfdc
Alumnot2 <Alumnot2@ORD21>
2020-11-09 14:59:39 +0100

    Agregar archivos de proyecto.

---
+++ ActividadUT3_2_Layout_13/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ActividadUT3_2_Layout_13
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void AnyadirNumero(object sender, RoutedEventArgs e)
+        {
+            (MainWindow as MainWindow).CajaSumadora.Text += (sender as Button).Tag.ToString();
+        }
+    }
+}
---
+++ ActividadUT3_2_Layout_13/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActividadUT3_2_Layout_13
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button uno = new Button
+            {
+                Content = "1",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "1"                
+            };
+
+            Button dos = new Button
+            {
+                Content = "2",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "2"
+            };
+
+            Button tres = new Button
+            {
+                Content = "3",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "3"
+            };
+
+            Button cuatro = new Button
+            {
+                Content = "4",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "4"
+            };
+
+            Button cinco = new Button
+            {
+                Content = "5",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "5"
+            };
+
+            Button seis = new Button
+            {
+                Content = "6",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "6"
+            };
+
+            Button siete = new Button
+            {
+                Content = "7",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "7"
+            };
+
+            Button ocho = new Button
+            {
+                Content = "8",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "8"
+            };
+
+            Button nueve = new Button
+            {
+                Content = "9",
+                Style = (Style)Application.Current.Resources["botones"],
+                Tag = "9"
+            };
+
+                    
+            contenedor.Children.Add(uno);
+            contenedor.Children.Add(dos);
+            contenedor.Children.Add(tres);
+            contenedor.Children.Add(cuatro);
+            contenedor.Children.Add(cinco);
+            contenedor.Children.Add(seis);
+            contenedor.Children.Add(siete);
+            contenedor.Children.Add(ocho);
+            contenedor.Children.Add(nueve);
+
+            Grid.SetRow(uno, 2); Grid.SetColumn(uno,0);
+            Grid.SetRow(dos, 2); Grid.SetColumn(dos,1);
+            Grid.SetRow(tres, 2); Grid.SetColumn(tres,2);
+
+            Grid.SetRow(cuatro,3); Grid.SetColumn(cuatro, 0);
+            Grid.SetRow(cinco, 3); Grid.SetColumn(cinco,1);
+            Grid.SetRow(seis, 3); Grid.SetColumn(seis,2);
+
+            Grid.SetRow(siete, 4); Grid.SetColumn(siete,0);
+            Grid.SetRow(ocho, 4); Grid.SetColumn(ocho,1);
+            Grid.SetRow(nueve, 4); Grid.SetColumn(nueve,2);
+        }
+    }
+}
